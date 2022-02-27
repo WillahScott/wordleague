bab57e7ed4b650af1306314f57d88319b6d5b687
Will <willie9monge@gmail.com>
2022-02-26 17:13:06 -0800

    added backend draft

---
+++ supabase-backend/README.md
@@ -0,0 +1,3 @@
+## Database Backend
+
+Hosted on Supabase.
---
+++ supabase-backend/tables/players.sql
@@ -0,0 +1,16 @@
+CREATE TABLE players (
+    id uuid references auth.users not null,
+    updated_at timestamp with time zone,
+    username text unique,
+    avatar_url text,
+
+    primary key (id),
+    unique(username),
+    constraint username_length check (char_length(username) >= 3)
+);
+
+ALTER TABLE players ENABLE ROW LEVEL SECURITY;
+
+CREATE POLICY "Players can only view, insert, or update their own profile."
+    ON players for SELECT, INSERT, UPDATE
+    WITH CHECK ( auth.uid() = id );
