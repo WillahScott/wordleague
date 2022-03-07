95a2e14c9ac6d9d564c132a2b9687fae967ae81e
Will <willie9monge@gmail.com>
2022-03-06 17:02:46 -0800

    main structure

--- wordleague-ui/src/routes/index.svelte
+++ wordleague-ui/src/routes/index.svelte
@@ -1,2 +1,30 @@
-<h1 class="text-3xl font-bold text-purple-800">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="py-10 relative h-screen flex flex-col items-center md:justify-center">
+	<h1 class="text-center text-4xl md:text-6xl font-bold text-purple-800 mb-14">
+		Welcome to <span class="border px-0.5 py-0 m-0 border-purple-900 bg-purple-700 text-purple-200"
+			>WordLeague</span
+		>
+	</h1>
+
+	<div class="flex flex-col items-center justify-around text-2xl text-purple-900 font-mono font-bold md:font-normal">
+		<p
+			class="px-5 py-1 my-2 rounded-sm bg-pink-400 hover:bg-purple-700 hover:text-purple-200 hover:cursor-pointer"
+		>
+			<a href="/solve">Solve Today's Challenges</a>
+		</p>
+		<p
+			class="px-5 py-1 my-2 rounded-sm bg-pink-400 hover:bg-purple-700 hover:text-purple-200 hover:cursor-pointer"
+		>
+			<a href="/set-challenges">Set Challenges for others</a>
+		</p>
+		<p
+			class="px-5 py-1 my-2 rounded-sm bg-pink-400 hover:bg-purple-700 hover:text-purple-200 hover:cursor-pointer"
+		>
+			<a href="/leagues">League Stats</a>
+		</p>
+		<p
+			class="px-5 py-1 my-2 rounded-sm bg-pink-400 hover:bg-purple-700 hover:text-purple-200 hover:cursor-pointer"
+		>
+			<a href="/profile">My Profile</a>
+		</p>
+	</div>
+</div>
---
+++ wordleague-ui/src/routes/leagues.svelte
@@ -0,0 +1,11 @@
+<div class="py-10 relative h-screen flex flex-col items-center md:justify-center">
+	<a href="/"
+		><h1 class="text-center text-4xl md:text-6xl font-bold text-purple-800 mb-14">
+			<span class="border px-0.5 py-0 m-0 border-purple-900 bg-purple-700 text-purple-200"
+				>WordLeague</span
+			>
+		</h1>
+	</a>
+
+	<h3 class="text-center text-2xl md:text-4xl font-bold font-mono text-purple-800">My Leagues</h3>
+</div>
