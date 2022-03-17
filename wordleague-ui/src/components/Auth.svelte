4ac6bcf1cb0060f69f61aa4454c47465e0fd7d18
Will <willie9monge@gmail.com>
2022-03-16 22:30:07 -0700

    added signup visual

--- wordleague-ui/src/components/Auth.svelte
+++ wordleague-ui/src/components/Auth.svelte
@@ -1,9 +1,13 @@
 <script>
 	import { supabase } from '$lib/supabase';
 	import Logo from './Logo.svelte';
+	import AppleSignIn from './signin/AppleSignin.svelte';
+	import GoogleSignIn from './signin/GoogleSignin.svelte';
 
-	let loading = false;
+	let showSignUp = false;
 	let email;
+	let password;
+	let signinSection;
 
 	const handleLogin = async () => {
 		try {
@@ -20,6 +24,11 @@
 		}
 	};
 
+	const goToSignUp = () => {
+		showSignUp = true;
+		signinSection.scrollTop({ behavior: 'smooth', block: 'end' });
+	};
+
 	const handleCreateAccount = async () => {};
 </script>
 
@@ -30,42 +39,100 @@
 
 	<div class="flex-grow flex flex-col items-center text-2xl text-purple-900">
 		<div class="flex flex-col items-center justify-around pt-2 pb-2 px-8 ">
-			<p class="text-2xl font-mono text-pink-400 mb-8">
+			<p class="text-2xl font-mono text-pink-400 mb-4">
 				Create your own word-guessing league and challenge your friends!
 			</p>
 		</div>
 		<div class="flex flex-col py-16 px-8 w-full">
 			<p class="text-2xl text-purple-600 font-mono">Returning users</p>
+			<div
+				class="mt-5 self-center max-w-2xl w-full flex flex-col md:flex-row justify-between items-center"
+			>
+				<GoogleSignIn width={'50px'} height={'50px'} />
+				<AppleSignIn width={'50px'} height={'50px'} />
+			</div>
 			<form class="my-6 flex flex-col justify-center" on:submit|preventDefault={handleLogin}>
-				<div class="flex flex-col text-sm mb-2">
+				<div class="flex flex-col md:flex-row text-sm mb-2">
 					<input
 						type="email"
 						name="email"
 						id="email"
 						bind:value={email}
 						placeholder="Your email"
-						class="text-purple-900 font-mono text-lg rounded-sm bg-purple-200 hover:ring-purple-500 focus:ring-purple-500"
+						class="md:mr-1 md:flex-1 text-purple-900 font-mono text-lg rounded-sm bg-purple-200 hover:ring-purple-500 focus:ring-purple-500"
+					/>
+					<input
+						type="password"
+						name="password"
+						id="password"
+						bind:value={password}
+						placeholder="Password"
+						class="mt-1 md:mt-0 md:ml-1 md:flex-1 text-purple-900 font-mono text-lg rounded-sm bg-purple-200 hover:ring-purple-500 focus:ring-purple-500"
 					/>
 				</div>
 				<button
 					type="submit"
 					class="w-full rounded-sm bg-purple-500 text-purple-900 text-xl uppercase font-mono font-bold py-2 px-10"
-					>Log In</button
+					>Sign In</button
 				>
 			</form>
 		</div>
 
-		<div class="mb-10 flex flex-col py-16 px-8 w-full">
+		<div class="mb-10 flex flex-col py-2 px-8 w-full">
 			<p class="text-2xl text-purple-600 font-mono">New users</p>
 			<form
 				class="my-6 flex flex-col justify-center"
 				on:submit|preventDefault={handleCreateAccount}
 			>
-				<button
-					type="submit"
-					class="w-full rounded bg-purple-500 text-purple-900 text-xl uppercase font-mono font-bold py-2 px-10"
-					>Create an Account</button
-				>
+				{#if !showSignUp}
+					<button
+						on:click={goToSignUp}
+						class="w-full rounded bg-purple-500 text-purple-900 text-xl uppercase font-mono font-bold py-2 px-10"
+						>Create an Account</button
+					>
+				{:else}
+					<div class="flex flex-col w-full pb-4">
+						<p class="mb-2 text-base md:text-lg text-pink-400 font-mono">
+							You can choose to simply sign up using one of the following providers...
+						</p>
+						<div
+							class=" self-center max-w-2xl w-full py-3 flex flex-col md:flex-row justify-between items-center"
+						>
+							<GoogleSignIn width={'50px'} height={'50px'} signup />
+							<AppleSignIn width={'50px'} height={'50px'} signup />
+						</div>
+						<div class="flex flex-col mt-4">
+							<p class="my-2 text-base md:text-lg text-pink-400 font-mono">
+								...or simply create an account with an email & password:
+							</p>
+
+							<div class="flex flex-col md:flex-row text-sm mb-2 mt-2">
+								<input
+									type="email"
+									name="email"
+									id="email"
+									bind:value={email}
+									placeholder="Your email"
+									class="md:mr-1 md:flex-1 text-purple-900 font-mono text-lg rounded-sm bg-purple-200 hover:ring-purple-500 focus:ring-purple-500"
+								/>
+								<input
+									type="password"
+									name="password"
+									id="password"
+									bind:value={password}
+									placeholder="Password"
+									class="mt-1 md:mt-0 md:ml-1 md:flex-1 text-purple-900 font-mono text-lg rounded-sm bg-purple-200 hover:ring-purple-500 focus:ring-purple-500"
+								/>
+							</div>
+							<button
+								bind:this={signinSection}
+								type="submit"
+								class="w-full rounded-sm bg-purple-500 text-purple-900 text-xl uppercase font-mono font-bold py-2 px-10"
+								>Sign Up with my email</button
+							>
+						</div>
+					</div>
+				{/if}
 			</form>
 		</div>
 	</div>
