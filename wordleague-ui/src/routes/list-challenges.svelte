3c7361785e2a914e189f5f2d139928fe3ba1b30a
Will <willie9monge@gmail.com>
2022-03-19 21:33:40 -0700

    minor visual adjustments

--- wordleague-ui/src/routes/list-challenges.svelte
+++ wordleague-ui/src/routes/list-challenges.svelte
@@ -2,7 +2,7 @@
 	import { challengeList } from '$lib/stores/challengeList';
 </script>
 
-<div class="py-10 relative h-screen flex flex-col items-center md:justify-center">
+<div class="py-10 relative h-screen flex flex-col items-center pt-20 md:pt-10 md:justify-center">
 	<h3 class="text-center text-xl md:text-4xl font-bold font-mono text-purple-800">
 		These are your challenges for today:
 	</h3>
@@ -16,11 +16,11 @@
 					<a href={`/solve?challenge=${challenge.id}`} key={challenge}>
 						<div class="flex flex-row items-center">
 							<div class="text-purple-300 font-serif mr-3">#{i + 1}</div>
-							<div class="flex-grow flex flex-row justify-between bg-purple-300 border-2 border-purple-800 text-purple-900 font-serif rounded-md p-2 my-5">
-								<div class="text-xl">
-									<span class="italic text-sm">by:</span> {challenge.creator}
+							<div class="flex-grow flex flex-col p-2 my-5 pl-8 sm:pl-2 sm:flex-row justify-between bg-purple-300 border-2 border-purple-800 text-purple-900 rounded-md">
+								<div class="text-xl font-mono flex items-center">
+									<span class="italic text-sm font-serif mr-2">by:</span> {challenge.creator}
 								</div>
-								<div class="text-lg">&#x1F3C6; {challenge.league}</div>
+								<div class="text-base font-mono flex items-center">&#x1F3C6; {challenge.league}</div>
 							</div>
 	
 						</div>
